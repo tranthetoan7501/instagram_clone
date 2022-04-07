b9d8a1ddff21d4901e6d2758cf2ccb7aaef676a6
Nguyễn Trường Phước <nt.phuoc1707@gmail.com>
2022-04-08 00:02:02 +0700

    Update story function

--- app/src/main/java/com/hcmus/mobilappsocialnetworkingimage/adapter/storiesAdapter.java
+++ app/src/main/java/com/hcmus/mobilappsocialnetworkingimage/adapter/storiesAdapter.java
@@ -22,8 +22,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.hcmus.mobilappsocialnetworkingimage.R;
 import com.hcmus.mobilappsocialnetworkingimage.activity.storiesActivity;
+import com.nostra13.universalimageloader.utils.L;
 import com.squareup.picasso.Picasso;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
@@ -31,12 +33,12 @@ import java.util.Vector;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class storiesAdapter extends RecyclerView.Adapter<storiesAdapter.storiesViewHolder> {
-    Vector<String> listImage;
+    List<List<String>> listImage;
     Vector<String> listName;
     Vector<String> listAvt;
     Context context;
 
-    public storiesAdapter(Vector<String> listImage, Vector<String> listName,Vector<String> listAvt, Context context) {
+    public storiesAdapter(List<List<String>> listImage, Vector<String> listName, Vector<String> listAvt, Context context) {
         this.listName = listName;
         this.listImage = listImage;
         this.context = context;
@@ -53,7 +55,8 @@ public class storiesAdapter extends RecyclerView.Adapter<storiesAdapter.storiesV
     @Override
     public void onBindViewHolder(@NonNull storiesViewHolder holder, @SuppressLint("RecyclerView") int position) {
         if(listName.isEmpty()) return;
-        Picasso.get().load(listImage.get(position)).into(holder.circleImageView);
+        System.out.println(listImage.get(position).get(0));
+        Picasso.get().load(listImage.get(position).get(0)).into(holder.circleImageView);
         holder.textView.setText(listName.get(position));
 
         holder.circleImageView.setOnClickListener(new View.OnClickListener() {
@@ -62,8 +65,9 @@ public class storiesAdapter extends RecyclerView.Adapter<storiesAdapter.storiesV
                 holder.circleImageView.setBorderColor(context.getColor(R.color.LightGrey));
                 Intent intent=new Intent(context, storiesActivity.class);
                 intent.putExtra("position",String.valueOf(position));
+                intent.putExtra("Image", (Serializable) listImage);
                 intent.putStringArrayListExtra("Name",new ArrayList<String>(listName));
-                intent.putStringArrayListExtra("Image",new ArrayList<String>(listImage));
+                //intent.putStringArrayListExtra("Image",new ArrayList<String>(listImage));
                 intent.putStringArrayListExtra("Avatar",new ArrayList<String>(listAvt));
                 context.startActivity(intent);
 
